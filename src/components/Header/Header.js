af1635061080e6e8dd53aaa0e9419cea1b58729c
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-17 13:53:48 +0600

    Update top-bar styles, search box, logo etc.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,27 @@ import Nav from '../Nav/Nav';
 
 const Header = () => {
   return (
-    <header className='w-full bg-slate-800'>
-      <h1 className='container mx-auto text-white'>Travel Geeks BD</h1>
+    <header className='w-full  bg-slate-800'>
+      <div className='top-bar container mx-auto flex justify-between items-center border-b border-b-[rgba(255,255,255,0.5)] shadow'>
+        <div className='main-logo w-1/5'>
+          <h1 className='text-orange-400 text-2xl font-extrabold'>
+            TravelGeeks<span className='text-white'>BD</span>
+          </h1>
+        </div>
+
+        <div className='search-box w-3/5 text-center px-4 py-4'>
+          <input
+            className='px-4 py-1 rounded-full w-4/6'
+            type='text'
+            name='search-box'
+            id='search-box'
+            placeholder='Search Anything'
+          />
+        </div>
+        <div className='social-icon w-1/5 text-right text-white'>
+          <a href='/'>FB</a>
+        </div>
+      </div>
       <Nav></Nav>
     </header>
   );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const navItems = [
   { id: 1, name: 'Home', ref: '/home' },
