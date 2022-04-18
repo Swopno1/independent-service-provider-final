685bf0b6e6a2d13d381848841fd1f15a8188ef78
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-18 23:14:13 +0600

    Leave this as it is not a requirement. Yet to solve login issue.

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import useServices from '../../hooks/useServices';
 
 const Checkout = () => {
-  const services = useServices([]);
-  let { serviceID } = useParams();
+  // const services = useServices([]);
+  // let { serviceID } = useParams();
 
-  useEffect(() => {
-    console.log(services);
-  }, [services]);
+  // useEffect(() => {
+  //   console.log(services);
+  // }, [services]);
 
-  console.log(serviceID);
+  // console.log(serviceID);
 
   return (
-    <div>
-      <h3>Checkout Page</h3>
+    <div className='container'>
+      <div>
+        <h1 className='text-center text-orange-500 text-5xl font-extrabold mt-6'>
+          <Link to='/'>
+            TravelGeeks<span className='text-black'>BD</span>
+          </Link>
+        </h1>
+        <h2 className='mt-12 text-center text-3xl font-extrabold text-gray-900'>
+          Checkout Page
+        </h2>
+      </div>
     </div>
   );
 };
